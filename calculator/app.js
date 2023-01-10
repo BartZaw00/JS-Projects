81535f083317c45576546bf8c0599899a841caee
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-01-10 14:02:59 +0100

    [Calculator] fixing the html issues

--- calculator/app.js
+++ calculator/app.js
@@ -10,6 +10,8 @@ const btnClear = document.querySelector('[data-clear]');
 
 const btnEquals = document.querySelector('[data-equals]');
 
+const btnHistoryClear = document.querySelector('[data-clear-history]');
+
 let result = '';
 
 let lastOperator = '';
@@ -35,7 +37,14 @@ let lastOperator = '';
 
 
 function displayNumber() {
-    console.log(`Button with text ${this.textContent} clicked`)
+    /* jezeli przycisk w jaki klikamy ma textContent === . oraz aktualny rezultat jest pusty */
+    if (this.textContent === '.' && actualResult.innerHTML === "")
+        return ".0";
+    /* jezeli przycisk w jaki klikamy ma textContent === . oraz aktualny rezultat zawiera "." */
+    if (this.textContent === '.' && actualResult.innerHTML.includes("."))
+        return
+
+    console.log('aktualny rezultat: ' + actualResult.innerHTML)
     /* '' <- javascript traktuje jako literalny ciąg znaków (string) */
     /* "" <- pozwala zawrzeć ${} w stringu */
     /* `` <- pozwala zawrzeć ${} w stringu */
@@ -43,48 +52,48 @@ function displayNumber() {
 }
 
 
-function operation() {
-    console.log(this.textContent)
-    if (this.textContent == '.' && actualResult.innerHTML == "")
-        actualResult.innerHTML += "0.";
-    if (actualResult.innerHTML == "")
-        return;
+function pickOperation() {
+    //     console.log(this.textContent)
+    //     if (this.textContent == '.' && actualResult.innerHTML == "")
+    //         actualResult.innerHTML += "0.";
+    //     if (actualResult.innerHTML == "")
+    //         return;
 
 
-    console.log((!isNaN(Number(actualResult.innerHTML.charAt(actualResult.innerHTML.length - 2)))))
-    if (!isNaN(actualResult.innerHTML.charAt(actualResult.innerHTML.length - 2)))
-        switch (this.textContent) {
-            case '+':
-                actualResult.innerHTML += ' + ';
-                break;
-            case '-':
-                actualResult.innerHTML += ' - ';
-                break;
-            case 'x':
-                actualResult.innerHTML += ' x ';
-                break;
-            case ':':
-                actualResult.innerHTML += ' : ';
-                break;
-            case '.':
-                if(actualResult.innerHTML.charAt(actualResult.innerHTML.length - 1) != '.')
-                actualResult.innerHTML += '.';
-                break;
-            default:
-                actualResult.innerHTML += ' error ';
-                break;
-        }
+    //     console.log((!isNaN(Number(actualResult.innerHTML.charAt(actualResult.innerHTML.length - 2)))))
+    //     if (!isNaN(actualResult.innerHTML.charAt(actualResult.innerHTML.length - 2)))
+    //         switch (this.textContent) {
+    //             case '+':
+    //                 actualResult.innerHTML += ' + ';
+    //                 break;
+    //             case '-':
+    //                 actualResult.innerHTML += ' - ';
+    //                 break;
+    //             case 'x':
+    //                 actualResult.innerHTML += ' x ';
+    //                 break;
+    //             case ':':
+    //                 actualResult.innerHTML += ' : ';
+    //                 break;
+    //             case '.':
+    //                 if (actualResult.innerHTML.charAt(actualResult.innerHTML.length - 1) != '.')
+    //                     actualResult.innerHTML += '.';
+    //                 break;
+    //             default:
+    //                 actualResult.innerHTML += ' error ';
+    //                 break;
+    //         }
 }
 
 function equalTo() {
-    result = actualResult.innerHTML.replace(/[:]/g, "/");
-    result = result.replace(/[x]/g, "*");
-    /* zamiana : oraz x na operacje matematyczne czyli / oraz * */
-    console.log("Aktualny rezultat: " + result)
-    previousEquation.innerHTML = actualResult.innerHTML + ' =';
-    // actualResult = ?
-    /* wykonanie operacji matematycznej na stringu */
-    /* NIE KORZYSTAĆ Z EVAL() !!! */
+    //     result = actualResult.innerHTML.replace(/[:]/g, "/");
+    //     result = result.replace(/[x]/g, "*");
+    //     /* zamiana : oraz x na operacje matematyczne czyli / oraz * */
+    //     console.log("Aktualny rezultat: " + result)
+    //     previousEquation.innerHTML = actualResult.innerHTML + ' =';
+    //     // actualResult = ?
+    //     /* wykonanie operacji matematycznej na stringu */
+    //     /* NIE KORZYSTAĆ Z EVAL() !!! */
 }
 
 function clearAll() {
@@ -95,7 +104,7 @@ function clearAll() {
 
 
 btnsOperator.forEach((button) => {
-    button.addEventListener('click', operation);
+    button.addEventListener('click', pickOperation);
 })
 
 btnEquals.addEventListener('click', equalTo);
