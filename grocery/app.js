fec4e1c3493eb1b448ae4f0eecad2d8f16d43e01
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-01-19 20:13:22 +0100

    [Grocery] clear-items button works + deleting every single item works, editing every item needs to be done

--- grocery/app.js
+++ grocery/app.js
@@ -1,15 +1,39 @@
 const formField = document.querySelector('[data-form]');
 const inputField = document.querySelector('[data-input]');
-const btnEdit = document.querySelectorAll('[data-edit]');
-const btnDelete = document.querySelectorAll('[data-delete');
+const list = document.querySelector('[data-list]');
+const clear = document.querySelector('[data-clear]');
 
-
+let btnsEdit = '';
+let btnsDelete = '';
 
 formField.addEventListener('submit', (e) => {
     e.preventDefault();
+
     console.log(inputField.value.match(/^\S*[a-zA-Z\s]\S*$/));
     // Only Alphabet Characters + spaces only between them, Not Empty
     if (inputField.value.match(/^\S*[a-zA-Z\s]\S*$/)) {
-        
+        if (clear.classList.contains('display-none')) clear.classList.remove('display-none');
+
+        list.innerHTML += `<li>${inputField.value}<div><button data-edit class="list-button edit">Edit</button><button data-delete class="list-button delete">Delete</button></div>
+        </li>`;
+        btnsEdit = document.querySelectorAll('[data-edit]');
+        btnsDelete = document.querySelectorAll('[data-delete]');
+        inputField.value = '';
+        btnsDelete.forEach(element => {
+            element.addEventListener('click', (item) => {
+                item.target.parentNode.parentNode.remove();
+                if(!list.children.length) clear.classList.add('display-none');
+            });
+            
+        });
     }
 });
+
+clear.addEventListener('click', () => {
+    if (!clear.classList.contains('display-none')) clear.classList.add('display-none');
+    list.innerHTML = ``;
+});
+
+
+
+
