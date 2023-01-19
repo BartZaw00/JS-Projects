32d243fc8dfd152c0e726c377a59735c799e6375
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-01-20 00:46:42 +0100

    [Grocery] edit list item still needs to be fixed

--- grocery/app.js
+++ grocery/app.js
@@ -5,6 +5,7 @@ const clear = document.querySelector('[data-clear]');
 
 let btnsEdit = '';
 let btnsDelete = '';
+let tempInput = '';
 
 formField.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -14,17 +15,34 @@ formField.addEventListener('submit', (e) => {
     if (inputField.value.match(/^\S*[a-zA-Z\s]\S*$/)) {
         if (clear.classList.contains('display-none')) clear.classList.remove('display-none');
 
-        list.innerHTML += `<li>${inputField.value}<div><button data-edit class="list-button edit">Edit</button><button data-delete class="list-button delete">Delete</button></div>
+        list.innerHTML += `<li><span>${inputField.value}</span><div><button data-edit class="list-button edit">Edit</button><button data-delete class="list-button delete">Delete</button></div>
         </li>`;
         btnsEdit = document.querySelectorAll('[data-edit]');
         btnsDelete = document.querySelectorAll('[data-delete]');
         inputField.value = '';
-        btnsDelete.forEach(element => {
-            element.addEventListener('click', (item) => {
-                item.target.parentNode.parentNode.remove();
-                if(!list.children.length) clear.classList.add('display-none');
-            });
-            
+
+        list.addEventListener('click', (event) => {
+            if (event.target.classList.contains('edit')) {
+                let li = event.target.parentNode.parentNode;
+                let span = li.childNodes[0];
+                let div = li.childNodes[1];
+                let input = document.createElement('input');
+                input.classList.add('edit-input');
+                let saveBtn = document.createElement('button');
+                saveBtn.classList.add('list-button');
+                saveBtn.textContent = 'Save';
+                input.value = span.textContent;
+                li.replaceChild(input, span);
+                li.replaceChild(saveBtn, div);
+            }
+        });
+
+        list.addEventListener('click', (event) => {
+            if (event.target.classList.contains('delete')) {
+                let li = event.target.parentNode.parentNode;
+                li.remove();
+                if (!list.children.length) clear.classList.add('display-none');
+            }
         });
     }
 });
@@ -33,7 +51,3 @@ clear.addEventListener('click', () => {
     if (!clear.classList.contains('display-none')) clear.classList.add('display-none');
     list.innerHTML = ``;
 });
-
-
-
-
