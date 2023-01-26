a01f81cd4983b8d3ea791ddc29ac8e62dd52b932
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-01-26 22:09:34 +0100

    [Quiz App] html + css started, responsiveness to do

---
+++ quiz-app/app.js
@@ -0,0 +1,103 @@
+const questions = [
+    {
+        id: 1,
+        question: 'Who won the 2018 FIFA World Cup?',
+        numOfAnswers: 4,
+        answer1: 'France',
+        answer2: 'Germany',
+        answer3: 'Brazil',
+        answer4: 'Spain',
+        correctAnswer: 1
+    },
+    {
+        id: 2,
+        question: 'Which country has won the most FIFA World Cups?',
+        numOfAnswers: 4,
+        answer1: 'Brazil',
+        answer2: 'Germany',
+        answer3: 'Italy',
+        answer4: 'Argentina',
+        correctAnswer: 1
+    },
+    {
+        id: 3,
+        question: 'Who is the all-time leading goal scorer in the UEFA Champions League?',
+        numOfAnswers: 4,
+        answer1: 'Lionel Messi',
+        answer2: 'Cristiano Ronaldo',
+        answer3: 'Raul',
+        answer4: 'Lewandowski',
+        correctAnswer: 2
+    },
+    {
+        id: 4,
+        question: 'Which club has won the most English Premier League titles?',
+        numOfAnswers: 4,
+        answer1: 'Manchester United',
+        answer2: 'Arsenal',
+        answer3: 'Chelsea',
+        answer4: 'Liverpool',
+        correctAnswer: 4
+    },
+    {
+        id: 5,
+        question: 'Which team has won the most Copa Libertadores titles?',
+        numOfAnswers: 4,
+        answer1: 'Boca Juniors',
+        answer2: 'River Plate',
+        answer3: 'Independiente',
+        answer4: 'Racing Club',
+        correctAnswer: 2
+    },
+    {
+        id: 6,
+        question: 'Who is the all-time leading goal scorer for the Brazilian national team?',
+        numOfAnswers: 4,
+        answer1: 'Pele',
+        answer2: 'Ronaldo',
+        answer3: 'Neymar',
+        answer4: 'Zico',
+        correctAnswer: 1
+    },
+    {
+        id: 7,
+        question: 'Which team has won the most UEFA Europa League titles?',
+        numOfAnswers: 4,
+        answer1: 'Chelsea',
+        answer2: 'Inter Milan',
+        answer3: 'Sevilla',
+        answer4: 'Atletico Madrid',
+        correctAnswer: 3
+    },
+    {
+        id: 8,
+        question: 'Which team has won the most UEFA Super Cup?',
+        numOfAnswers: 4,
+        answer1: 'Ajax',
+        answer2: 'Real Madrid',
+        answer3: 'Barcelona',
+        answer4: 'AC Milan',
+        correctAnswer: 1
+    },
+    {
+        id: 9,
+        question: 'Which team has won the most Inter-Continental Cup?',
+        numOfAnswers: 4,
+        answer1: 'River Plate',
+        answer2: 'Real Madrid',
+        answer3: 'Boca Juniors',
+        answer4: 'AC Milan',
+        correctAnswer: 4
+    },
+    {
+        id: 10,
+        question: 'Which team has won the most FIFA Club World Cup?',
+        numOfAnswers: 4,
+        answer1: 'Real Madrid',
+        answer2: 'Barcelona',
+        answer3: 'Manchester United',
+        answer4: 'Bayern Munich',
+        correctAnswer: 2
+    },
+];
+
