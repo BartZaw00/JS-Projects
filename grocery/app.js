c2ec82a89e5cf73ff869e574a56ed88746bd8b79
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-02-08 00:50:19 +0100

    [Grocery] editing and saving list element bug fixed

--- grocery/app.js
+++ grocery/app.js
@@ -10,12 +10,9 @@ let btnsDelete;
 let tempInput;
 
 // list event listener variables
-let li;
-let currentLi;
-let span;
 let editBtn;
-let input;
 let saveBtn;
+
 
 formField.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -23,7 +20,8 @@ formField.addEventListener('submit', (e) => {
     console.log(inputField.value.match(/^\S*[a-zA-Z\s]\S*$/));
     // Only Alphabet Characters + spaces only between them, Not Empty
     if (inputField.value.match(/^\S*[a-zA-Z\s]\S*$/)) {
-        if (clear.classList.contains('display-none')) clear.classList.remove('display-none');
+        if (clear.classList.contains('display-none'))
+            clear.classList.remove('display-none');
 
         list.innerHTML += `<li><span>${inputField.value}</span><div><button data-edit class="list-button edit">Edit</button><button data-delete class="list-button delete">Delete</button></div>
         </li>`;
@@ -31,53 +29,59 @@ formField.addEventListener('submit', (e) => {
         btnsDelete = document.querySelectorAll('[data-delete]');
         inputField.value = '';
     }
+
+    Array.from(list.children).forEach((listItem) => {
+        listItem.addEventListener('click', (event) => {
+            if (event.target.classList.contains('edit')) {
+                for (let i = 0; i < list.children.length; i++) {
+                    if (list.children[i].children[1].children[0].classList.contains('save')) {
+                        inputToSpan(list.children[i]);
+                        saveToEdit(list.children[i].children[1].children[0]);
+                    }
+                }
+                spanToInput(event.target.parentNode.parentNode);
+                editToSave(event.target)
+            }
+            else if (event.target.classList.contains('save')) {
+                inputToSpan(event.target.parentNode.parentNode)
+                saveToEdit(event.target);
+            }
+            else if (event.target.classList.contains('delete')) {
+                const li = event.target.parentNode.parentNode;
+                li.remove();
+                if (list.children.length === 0)
+                    clear.classList.add('display-none');
+            }
+        });
+    });
 });
 
-list.addEventListener('click', (event) => {
-    console.log(event.target)
 
-    let listChildren = list.children;
 
-    for (let i = 0; i < list.children.length; i++) {
-        if (listChildren[i].children[1].children[0].classList.contains('save')) {
-            listChildren[i].children[1].children[0].classList.remove('save');
-            listChildren[i].children[1].children[0].classList.add('edit');
-            listChildren[i].children[1].children[0].textContent = 'Edit';
-            listChildren[i].children[0].textContent = input.value;
-            listChildren[i].children[0].replaceChild(span, input);
-        }
-    }
-    if (event.target.classList.contains('edit')) {
-        console.log('EDIT')
-        li = event.target.parentNode.parentNode;
-        span = li.childNodes[0];
-        editBtn = li.childNodes[1].childNodes[0];
-        input = document.createElement('input');
-        input.classList.add('edit-input');
-        editBtn.classList.remove('edit');
-        editBtn.classList.add('save');
-        editBtn.textContent = 'Save';
-        input.value = span.textContent;
-        li.replaceChild(input, span);
-    }
-    else if (event.target.classList.contains('save')) {
-        li = event.target.parentNode.parentNode;
-        input = li.childNodes[0];
-        editBtn = li.childNodes[1].childNodes[0];
-        editBtn.classList.remove('save');
-        editBtn.classList.add('edit');
-        editBtn.textContent = 'Edit';
-        console.log(editBtn)
-        span.textContent = input.value;
-        li.replaceChild(span, input);
-    }
-    else if (event.target.classList.contains('delete')) {
-        console.log('DELETE')
-        li = event.target.parentNode.parentNode;
-        li.remove();
-        if (!list.children.length) clear.classList.add('display-none');
-    }
-});
+function spanToInput(listElement) {
+    const inputElement = document.createElement('input');
+    inputElement.classList.add('edit-input');
+    inputElement.value = listElement.children[0].textContent;
+    listElement.replaceChild(inputElement, listElement.children[0]);
+}
+
+function inputToSpan(listElement) {
+    const spanElement = document.createElement('span');
+    spanElement.textContent = listElement.children[0].value;
+    listElement.replaceChild(spanElement, listElement.children[0]);
+}
+
+function saveToEdit(element) {
+    element.classList.remove('save');
+    element.classList.add('edit');
+    element.textContent = 'Edit';
+}
+
+function editToSave(element) {
+    element.classList.remove('edit');
+    element.classList.add('save');
+    element.textContent = 'Save';
+}
 
 clear.addEventListener('click', () => {
     if (!clear.classList.contains('display-none')) clear.classList.add('display-none');
