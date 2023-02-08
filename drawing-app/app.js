5bebce1b07b4c1ffb7bb750545739ee86adf2f78
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-02-08 17:24:47 +0100

    [Drawing App] picking a color works

--- drawing-app/app.js
+++ drawing-app/app.js
@@ -2,6 +2,8 @@ const pencilSize = document.querySelector('[data-size]');
 const minusSize = document.querySelector('[data-size-minus]');
 const plusSize = document.querySelector('[data-size-plus]');
 
+const colors = document.querySelectorAll('[data-colors]');
+
 const drawingArea = document.getElementById('canvas');
 const ctx = drawingArea.getContext('2d');
 
@@ -24,6 +26,34 @@ drawingArea.addEventListener('mousedown', startDrawing);
 drawingArea.addEventListener('mousemove', draw);
 
 drawingArea.addEventListener('mouseup', stopDrawing);
+
+colors.forEach((color) => {
+    color.addEventListener('click', (event) => {
+        if (event.target.classList.contains('black'))
+            ctx.fillStyle = 'black';
+        if (event.target.classList.contains('brown'))
+            ctx.fillStyle = 'brown';
+        if (event.target.classList.contains('grey'))
+            ctx.fillStyle = 'grey';
+        if (event.target.classList.contains('purple'))
+            ctx.fillStyle = 'purple';
+        if (event.target.classList.contains('pink'))
+            ctx.fillStyle = 'pink';
+        if (event.target.classList.contains('red'))
+            ctx.fillStyle = 'red';
+        if (event.target.classList.contains('green'))
+            ctx.fillStyle = 'green';
+        if (event.target.classList.contains('blue'))
+            ctx.fillStyle = 'blue';
+        if (event.target.classList.contains('orange'))
+            ctx.fillStyle = 'orange';
+        if (event.target.classList.contains('yellow'))
+            ctx.fillStyle = 'yellow';
+        if (event.target.classList.contains('white'))
+            ctx.fillStyle = 'white';
+
+    });
+});
 
 
 function startDrawing(e) {
